109ffa8c38122471a52cba10af381e54ceaf70b4
eugene.zabolotny <eugene.zabolotny@gmail.com>
2018-06-18 16:41:20 +0300

    pipes

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -10,18 +10,20 @@ export class CarsComponent implements OnInit {
   carName = '';
   carStatus = false;
   cars = ['Ford', 'Audi', 'BMW', 'Mazda', 'Lanos', 'Bentley'];
+  dates = [
+    new Date(2015, 3, 4),
+    new Date(2011, 2, 5),
+    new Date(2017, 4, 6),
+    new Date(2000, 1, 7),
+  ];
+
+  constructor() {
+  }
 
   addCar() {
     this.carStatus = true;
     this.cars.push(this.carName);
     this.carName = '';
-  }
-
-  setBigCarText(car: string) {
-    return car.length > 4;
-  }
-
-  constructor() {
   }
 
   ngOnInit() {
